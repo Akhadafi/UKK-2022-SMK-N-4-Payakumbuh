f844f6bb50a5996e20d8d989867dc2641bc4e64a
Akhadafi <khadafiar495@gmail.com>
2022-05-04 12:15:36 +0700

    Update query.php

--- query.php
+++ query.php
@@ -225,6 +225,8 @@ $field = mysqli_query($conn, "SELECT * FROM nama_tabel WHERE id = $id");
 
 <!-- Hapus -->
 <?php
+$id = $_GET["id"];
+
 function hapus($id)
 {
   global $conn;
@@ -232,7 +234,6 @@ function hapus($id)
   return mysqli_affected_rows($conn);
 }
 
-$id = $_GET["id"];
 
 if (hapus($id) > 0) {
   echo "
@@ -251,3 +252,104 @@ if (hapus($id) > 0) {
 }
 ?>
 <!-- Hapus -->
+
+
+<!-- Login -->
+<?php
+// cek cookie
+if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
+  $id = $_COOKIE['id'];
+  $key = $_COOKIE['key'];
+
+  // ambil username berdasarkan id
+  $result = mysqli_query($conn, "SELECT username FROM user WHERE id = $id");
+  $row = mysqli_fetch_assoc($result);
+
+  // cek cookie dan username
+  if ($key === hash('sha256', $row['username'])) {
+    $_SESSION['login'] = true;
+  }
+}
+
+if (isset($_SESSION["login"])) {
+  header("Location: index.php");
+  exit;
+}
+
+
+if (isset($_POST["login"])) {
+
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
+
+  // cek username
+  if (mysqli_num_rows($result) === 1) {
+
+    // cek password
+    $row = mysqli_fetch_assoc($result);
+    if (password_verify($password, $row["password"])) {
+      // set session
+      $_SESSION["login"] = true;
+
+      // cek remember me
+      if (isset($_POST['remember'])) {
+        // buat cookie
+        setcookie('id', $row['id'], time() + 60);
+        setcookie('key', hash('sha256', $row['username']), time() + 60);
+      }
+
+      header("Location: index.php");
+      exit;
+    }
+  }
+
+  $error = true;
+}
+?>
+
+
+<?php if (isset($error)) : ?>
+  <p style="color: red; font-style: italic;">username / password salah</p>
+<?php endif; ?>
+
+<form action="" method="post">
+
+  <ul>
+    <li>
+      <label for="username">Username :</label>
+      <input type="text" name="username" id="username">
+    </li>
+    <li>
+      <label for="password">Password :</label>
+      <input type="password" name="password" id="password">
+    </li>
+    <li>
+      <input type="checkbox" name="remember" id="remember">
+      <label for="remember">Remember me</label>
+    </li>
+    <li>
+      <button type="submit" name="login">Login</button>
+    </li>
+  </ul>
+
+</form>
+<!-- Login -->
+
+
+<!-- Logout -->
+<?php
+session_start();
+$_SESSION = [];
+session_unset();
+session_destroy();
+
+setcookie('id', '', time() - 3600);
+setcookie('key', '', time() - 3600);
+
+header("Location: login.php");
+exit;
+
+?>
+<!-- Logout -->
